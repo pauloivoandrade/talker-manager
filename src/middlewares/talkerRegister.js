d9028a32bd21e9fe3a6eae270ffe32faa2cf19c3
pauloivoandrade <pauloivoasantos@gmail.com>
2023-12-11 18:18:46 -0300

    endpoint PUT /talker/:id OK

--- src/middlewares/talkerRegister.js
+++ src/middlewares/talkerRegister.js
@@ -8,9 +8,9 @@ const talkerRegister = async (request, response) => {
   const newId = { id: talkersDatabase.length + 1 };
   const newTalker = Object.assign(newId, talkerRequest);
   talkersDatabase.push(newTalker);
-  const newDatabase = JSON.stringify(talkersDatabase);
+  // const newDatabase = JSON(talkersDatabase);
 
-  await writeTalker(newDatabase);
+  await writeTalker(talkersDatabase);
   return response.status(201).json(newTalker).end();
 };
     
